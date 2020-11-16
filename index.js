bbefbd893fecea33dc510d375ec881a4d2698bac
Jack Oldfield <30291237+JackOldfield@users.noreply.github.com>
2020-11-16 17:03:37 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, Notification } = require('electron')
+
+require('update-electron-app')({
+  repo: 'PrebuiltComputersUK/DesktopApplication',
+  updateInterval: '10 minutes',
+  logger: require('electron-log'),
+  notifyUser: 'true'
+})
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+function showNotification () {
+  const notification = {
+    title: 'Basic Notification',
+    body: 'Notification from the Main process'
+  }
+  new Notification(notification).show()
+}
+
+app.whenReady().then(createWindow).then(showNotification)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
